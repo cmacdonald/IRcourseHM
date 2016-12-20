190e31f38be9aff29fbb1c5c115ef42431e53dd6
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2016-12-20 13:42:48 +0000

    Clarify members

--- src/main/java/uk/ac/gla/dcs/models/MyWeightingModel.java
+++ src/main/java/uk/ac/gla/dcs/models/MyWeightingModel.java
@@ -11,6 +11,16 @@ public class MyWeightingModel extends WeightingModel
 	public String getInfo() { return this.getClass().getSimpleName(); }
 	
 	public double score(double tf, double docLength) {
+		//you should implement this method to return a score for a term occurring tf times in a document of docLength tokens.
+
+		//you may assume access to the following member variables of the superclass:
+		//averageDocumentLength (numberOfTokens /numberOfDocuments )
+   		//keyFrequency (The frequency of the term in the query)
+   		//documentFrequency (The document frequency of the term in the collection)
+   		//termFrequency (The term frequency in the collection)
+   		//numberOfDocuments (The number of documents in the collection)
+   		//numberOfTokens (the total length of all documents in the collection)
+
 		return 0d;
 	}
 
