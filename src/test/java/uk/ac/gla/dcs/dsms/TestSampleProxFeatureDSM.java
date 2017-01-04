cc028f3e8d0a40aeb33ef6e83d218bbd027881a5
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2017-01-04 12:10:35 +0000

    Refinements to unit tests

--- src/test/java/uk/ac/gla/dcs/dsms/TestSampleProxFeatureDSM.java
+++ src/test/java/uk/ac/gla/dcs/dsms/TestSampleProxFeatureDSM.java
@@ -1,15 +1,16 @@
 package uk.ac.gla.dcs.dsms;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
 import org.junit.Test;
-
-import org.terrier.tests.ApplicationSetupBasedTest;
 import org.terrier.indexing.IndexTestUtils;
-import org.terrier.utility.ApplicationSetup;
 import org.terrier.structures.Index;
+import org.terrier.structures.postings.BlockPosting;
 import org.terrier.structures.postings.IterablePosting;
-
-import uk.ac.gla.dcs.dsms.SampleProxFeatureDSM;
+import org.terrier.tests.ApplicationSetupBasedTest;
+import org.terrier.utility.ApplicationSetup;
 
 public class TestSampleProxFeatureDSM extends ApplicationSetupBasedTest
 {
@@ -21,7 +22,7 @@ public class TestSampleProxFeatureDSM extends ApplicationSetupBasedTest
 				new String[]{"doc1"}, 
 				new String[]{"The quick brown fox jumps over the lazy dog"});
 
-		//get posting iterators for two terms
+		//get posting iterators for two terms 'fox' and 'jumps'
 		IterablePosting[] ips = new IterablePosting[2];
 		ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
 		ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("jumps"));
@@ -29,6 +30,8 @@ public class TestSampleProxFeatureDSM extends ApplicationSetupBasedTest
 		ips[1].next();
 		assertEquals(0, ips[0].getId());
 		assertEquals(0, ips[1].getId());
+		System.out.println("Positions of term 'fox'="+ Arrays.toString( ((BlockPosting)ips[0]).getPositions()));
+		System.out.println("Positions of term 'jumps'="+ Arrays.toString( ((BlockPosting)ips[1]).getPositions()));
 
 		SampleProxFeatureDSM sample = new SampleProxFeatureDSM();
 		double score = sample.calculateDependence(
@@ -36,6 +39,8 @@ public class TestSampleProxFeatureDSM extends ApplicationSetupBasedTest
             new boolean[]{true,true},  //is this posting list on the correct document?
             new double[]{1d,1d}, false//doesnt matter
 		);
-		//TODO: check your score is correct
+		System.out.println(score);
+		//TODO: make your assertion about what the score should be
+		//assertEquals(XXX, score, 0.0d);
 	}
 }
--- src/test/java/uk/ac/gla/dcs/models/TestMyWeightingModel.java
+++ src/test/java/uk/ac/gla/dcs/models/TestMyWeightingModel.java
@@ -1,10 +1,38 @@
 package uk.ac.gla.dcs.models;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+import org.terrier.indexing.IndexTestUtils;
+import org.terrier.matching.models.WeightingModel;
+import org.terrier.structures.Index;
+import org.terrier.tests.ApplicationSetupBasedTest;
+import org.terrier.utility.ApplicationSetup;
 
-public class TestMyWeightingModel
+public class TestMyWeightingModel extends ApplicationSetupBasedTest
 {
-	@Test public void testModel()
+	@Test public void testModel() throws Exception
 	{
+		//no stemming or stopwords
+		ApplicationSetup.setProperty("termpipelines", "");
+		//make an index with a two sample documents		
+		Index index = IndexTestUtils.makeIndex(
+				new String[]{"doc1", "doc2"}, 
+				new String[]{"The quick brown fox jumps over the lazy dog", 
+					"Exploring the zoo, we saw every kangaroo jump and quite a few carried babies."});
+		
+		WeightingModel wm = new MyWeightingModel();
+		wm.setCollectionStatistics(index.getCollectionStatistics());
+		//double check: our index has two documents
+		assertEquals(2, index.getCollectionStatistics().getNumberOfDocuments());
+		
+		//what score would your weighting model give to the term 'jumps', 
+		// if it occurred once in a document with total length 5?
+		wm.setEntryStatistics(index.getLexicon().getLexiconEntry("jumps"));
+		wm.setKeyFrequency(1.0d);
+		wm.prepare();
+		double score = wm.score(1, 5);
+		System.out.println(score);
+		//TODO: make your assertion about what the score should be
+		//assertEquals(XXX, score, 0.0d);
 	}
 }
