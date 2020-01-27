6ea113fe22dc7597b0504010da25941781d80dca
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-01-27 18:48:06 +0000

    revised templates

--- src/main/java/uk/ac/gla/dcs/models/MyWeightingModel.java
+++ src/main/java/uk/ac/gla/dcs/models/MyWeightingModel.java
@@ -1,10 +1,11 @@
 package uk.ac.gla.dcs.models;
 
 import org.terrier.matching.models.WeightingModel;
+import org.terrier.structures.postings.Posting;
 
 /** You should use this sample class to implement a Simple TF*IDF weighting model for Exercise 1
   * of the exercise. You can tell Terrier to use your weighting model by specifying the 
-  * property trec.model=uk.ac.gla.dcs.models.MyWeightingModel
+  * -w commandline option, or the property trec.model=uk.ac.gla.dcs.models.MyWeightingModel.
   * NB: There is a corresponding unit test that you should also complete to test your model.
   * @author TODO
   */
@@ -14,7 +15,34 @@ public class MyWeightingModel extends WeightingModel
 
 	public String getInfo() { return this.getClass().getSimpleName(); }
 	
-	public double score(double tf, double docLength) {
+	boolean init = false;
+	void init() {
+		//you may complete any initialisation code here.
+		//you may assume access to 
+		//averageDocumentLength (numberOfTokens /numberOfDocuments )
+   		//keyFrequency (The frequency of the term in the query)
+   		//documentFrequency (The document frequency of the term in the collection)
+   		//termFrequency (The frequency of the term in the collection)
+   		//numberOfDocuments (The number of documents in the collection)
+		//numberOfTokens (the total length of all documents in the collection)
+
+		//rq.getIndex() (the underlying Index)
+
+		//rq.getRequest().getMatchingTerms() (the MatchingQueryTerms object, 
+		//which i.e. the system's low level representation of the query)
+		
+		//Terrier will only have one index loaded at the once time, so
+		//to share variables between weighting model instances, use static variables
+		init = true;
+	}
+
+	@Override
+	public double score(Posting p) {
+		if (! init)
+			init();
+
+		double tf = p.getFrequency();
+		double docLength = p.getDocumentLength();
 		//you should implement this method to return a score for a term occurring tf times in a document of docLength tokens.
 
 		//you may assume access to the following member variables of the superclass:
@@ -23,9 +51,16 @@ public class MyWeightingModel extends WeightingModel
    		//documentFrequency (The document frequency of the term in the collection)
    		//termFrequency (The frequency of the term in the collection)
    		//numberOfDocuments (The number of documents in the collection)
-   		//numberOfTokens (the total length of all documents in the collection)
+		//numberOfTokens (the total length of all documents in the collection)
+		//as well as any member variables you create   
+
 
 		return 0d;
+	}
+
+	@Override
+	public double score(double tf, double docLength) {
+		throw new UnsupportedOperationException("other method is in use");
 	}
 
 }
--- src/test/java/uk/ac/gla/dcs/models/TestMyWeightingModel.java
+++ src/test/java/uk/ac/gla/dcs/models/TestMyWeightingModel.java
@@ -1,37 +1,42 @@
 package uk.ac.gla.dcs.models;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.terrier.indexing.IndexTestUtils;
 import org.terrier.matching.models.WeightingModel;
+import org.terrier.querying.ManagerFactory;
+import org.terrier.querying.Request;
+import org.terrier.querying.SearchRequest;
 import org.terrier.structures.Index;
+import org.terrier.structures.postings.BasicPostingImpl;
 import org.terrier.tests.ApplicationSetupBasedTest;
 import org.terrier.utility.ApplicationSetup;
 
-public class TestMyWeightingModel extends ApplicationSetupBasedTest
-{
-	@Test public void testModel() throws Exception
-	{
-		//no stemming or stopwords
+public class TestMyWeightingModel extends ApplicationSetupBasedTest {
+	@Test
+	public void testModel() throws Exception {
+		// no stemming or stopwords
 		ApplicationSetup.setProperty("termpipelines", "");
-		//make an index with a two sample documents		
-		Index index = IndexTestUtils.makeIndex(
-				new String[]{"doc1", "doc2", "doc3"}, 
-				new String[]{"The quick brown fox jumps over the lazy dog", 
-					"Exploring the zoo, we saw every kangaroo jump and quite a few carried babies.",
-					"Sneaky white tiger creeps up on a visitor at a zoo before launching a lightning attack"});
-		
+		// make an index with a two sample documents
+		Index index = IndexTestUtils.makeIndex(new String[] { "doc1", "doc2", "doc3" },
+				new String[] { "The quick brown fox jumps over the lazy dog",
+						"Exploring the zoo, we saw every kangaroo jump and quite a few carried babies.",
+						"Sneaky white tiger creeps up on a visitor at a zoo before launching a lightning attack" });
+
 		WeightingModel wm = new MyWeightingModel();
 		wm.setCollectionStatistics(index.getCollectionStatistics());
-		//double check: our index has two documents
+		// double check: our index has two documents
 		assertEquals(3, index.getCollectionStatistics().getNumberOfDocuments());
+		SearchRequest srq = ManagerFactory.from(index.getIndexRef()).newSearchRequestFromQuery("jumps");
 		
-		//what score would your weighting model give to the term 'jumps', 
+		// what score would your weighting model give to the term 'jumps',
 		// if it occurred once in a document with total length 5?
 		wm.setEntryStatistics(index.getLexicon().getLexiconEntry("jumps"));
 		wm.setKeyFrequency(1.0d);
 		wm.prepare();
-		double score = wm.score(1, 5);
+		wm.setRequest((Request)srq);
+		double score = wm.score(new BasicPostingImpl(1, 5));
 		System.out.println(score);
 		//TODO: make your assertion about what the score should be
 		//assertEquals(XXX, score, 0.0d);
